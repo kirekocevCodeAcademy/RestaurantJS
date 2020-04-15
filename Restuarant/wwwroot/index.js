c2f713c577c4927ab40c932eb4f8825bf0ed8dfd
Kire Kocev <kkocev@yprime.com>
2020-04-15 18:37:49 +0200

    Part 1.

--- Restuarant/wwwroot/index.js
+++ Restuarant/wwwroot/index.js
@@ -1,6 +1,25 @@
-﻿let promise = $.get("http://kirekocev.no-ip.biz/api/Restaurants");
+﻿//let promise = $.get("http://kirekocev.no-ip.biz/api/Restaurant");
+//promise.then(
+//    data => console.log('success', data),
+//    error => console.log('error', error)
+//);
+
+let dataToPost = {
+    name: 'Post 1',
+    location: 'New App',
+    cuisine: 1
+};
+
+let promise = $.ajax({
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    type: "POST",
+    url: "http://kirekocev.no-ip.biz/api/Restaurants",
+    data: JSON.stringify(dataToPost),
+    dataType: "json"
+});
 promise.then(
     data => console.log('success', data),
     error => console.log('error', error)
 );
-
